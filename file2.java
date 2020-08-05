9dad1e278657c94bd3037dfe05c2442e44bf9066
Sanjay <sanjay@Sanjays-MBP.attlocal.net>
2020-08-05 16:51:17 -0500

    my first commit of source code

---
+++ file2.java
@@ -0,0 +1,20 @@
+total 45
+drwxrwxr-x+ 43 root  admin  1462 Jul 18 22:46 Applications
+drwxr-xr-x+ 62 root  wheel  2108 Jul 29 14:51 Library
+drwxr-xr-x@  2 root  wheel    68 Nov 23  2017 Network
+drwxr-xr-x@  4 root  wheel   136 Oct 25  2017 System
+drwxr-xr-x   5 root  admin   170 Nov 23  2017 Users
+drwxr-xr-x@  3 root  wheel   102 Jul 29 14:48 Volumes
+drwxr-xr-x@ 38 root  wheel  1292 Mar 26 17:45 bin
+drwxrwxr-t@  2 root  admin    68 Nov 23  2017 cores
+dr-xr-xr-x   3 root  wheel  4298 Jul 29 14:28 dev
+lrwxr-xr-x@  1 root  wheel    11 Nov 23  2017 etc -> private/etc
+dr-xr-xr-x   2 root  wheel     1 Jul 29 14:56 home
+-rw-r--r--@  1 root  wheel   313 Aug 31  2017 installer.failurerequests
+dr-xr-xr-x   2 root  wheel     1 Jul 29 14:56 net
+drwxr-xr-x@  3 root  wheel   102 Jul 18 23:27 opt
+drwxr-xr-x@  6 root  wheel   204 Nov 23  2017 private
+drwxr-xr-x@ 63 root  wheel  2142 Mar 26 17:45 sbin
+lrwxr-xr-x@  1 root  wheel    11 Nov 23  2017 tmp -> private/tmp
+drwxr-xr-x@ 10 root  wheel   340 Jul 29 14:51 usr
+lrwxr-xr-x@  1 root  wheel    11 Nov 23  2017 var -> private/var
---
+++ file3.java
@@ -0,0 +1,20 @@
+total 45
+drwxrwxr-x+ 44 root  admin  1496 Aug  5 16:11 Applications
+drwxr-xr-x+ 62 root  wheel  2108 Jul 29 14:51 Library
+drwxr-xr-x@  2 root  wheel    68 Nov 23  2017 Network
+drwxr-xr-x@  4 root  wheel   136 Oct 25  2017 System
+drwxr-xr-x   5 root  admin   170 Nov 23  2017 Users
+drwxr-xr-x@  4 root  wheel   136 Aug  5 16:11 Volumes
+drwxr-xr-x@ 38 root  wheel  1292 Mar 26 17:45 bin
+drwxrwxr-t@  2 root  admin    68 Nov 23  2017 cores
+dr-xr-xr-x   3 root  wheel  4387 Aug  4 17:44 dev
+lrwxr-xr-x@  1 root  wheel    11 Nov 23  2017 etc -> private/etc
+dr-xr-xr-x   2 root  wheel     1 Aug  5 16:24 home
+-rw-r--r--@  1 root  wheel   313 Aug 31  2017 installer.failurerequests
+dr-xr-xr-x   2 root  wheel     1 Aug  5 16:24 net
+drwxr-xr-x@  3 root  wheel   102 Aug  4 16:15 opt
+drwxr-xr-x@  6 root  wheel   204 Nov 23  2017 private
+drwxr-xr-x@ 63 root  wheel  2142 Mar 26 17:45 sbin
+lrwxr-xr-x@  1 root  wheel    11 Nov 23  2017 tmp -> private/tmp
+drwxr-xr-x@ 10 root  wheel   340 Jul 29 14:51 usr
+lrwxr-xr-x@  1 root  wheel    11 Nov 23  2017 var -> private/var
